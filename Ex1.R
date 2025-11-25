594571cf4f0d5faf842377540818cfa931a77552
LeonidReder <redel03@zedat.fu-berlin.de>
2025-11-25 12:09:58 +0000

    Task 1 completed

--- Ex1.R
+++ Ex1.R
@@ -1,4 +1,5 @@
-# Preamble
+# Preamble ----------------------------------------------------------------
+
 rm(list = ls())
 install <- TRUE
 if (install) {
@@ -23,8 +24,7 @@ sp5 <- sp5 %>%
   mutate(Date = as.Date(Date))
 
 
-# Task 1
-# a) 
+# a) ----------------------------------------------------------------------
 
 plot_nom_sp5 <- sp5 %>% ggplot(
   aes(x = Date, y = sp500)) +
@@ -36,7 +36,7 @@ plot_nom_sp5 <- sp5 %>% ggplot(
 
 plot_nom_sp5
 
-# b) 
+# b) ----------------------------------------------------------------------
 
 sp5 <- sp5 %>%
   mutate(log_sp5 = 100 * log(sp500))
@@ -53,4 +53,100 @@ plot_index_log
 
 # log-scaling helps interpret constant growth and compare recessions
 
-# c)
+
+# c) ----------------------------------------------------------------------
+
+sp5 <- sp5 %>%
+  mutate(log_diff_sp5 = c(NA, diff(log_sp5)))
+
+plot_index_dlog <- sp5 %>%
+  ggplot(aes(x = Date, y = log_diff_sp5)) +
+  geom_line(linewidth = 0.8, colour = col_pal[["red"]]) +
+  scale_x_date(date_breaks = "20 years", date_labels = "%Y") +
+  xlab(NULL) + ylab(NULL) +
+  ggtitle("Log Differences of the S&P500 Monthly Index") +
+  theme_bw()
+
+ggplotly(plot_index_dlog)
+?ggplotly
+
+#     - log differences approximates percent changes from month to month (for small values)
+#     - stabilizes the variance of time series where the variable experiences exponential growth
+#     - thus, makes the series more stable and stationary, which helps for computing AR(I)MA models
+
+
+# d) ----------------------------------------------------------------------
+
+# First, compute the percent changes of sp500 manually
+
+sp5 <-  sp5 %>%
+  mutate(percent_delta_sp5 = (sp500 / dplyr::lag(sp500, 1) - 1) * 100)
+
+plot_index_pct_chg <- sp5 %>%
+  ggplot(aes(x = Date, y = percent_delta_sp5)) +
+  geom_line(linewidth = 0.8, colour = col_pal[["orange"]]) +
+  scale_x_date(date_breaks = "20 years", date_labels = "%Y") +
+  xlab(NULL) + ylab(NULL) +
+  ggtitle("Percentage Change of S&P500 Monthly Index") +
+  theme_bw()
+
+ggplotly(plot_index_pct_chg)
+  
+# Comparison plot
+
+plot_compare_chg <- sp5 %>% 
+  ggplot(aes(x = Date)) +
+  # Add line for the log difference (alpha for transparency)
+  geom_line(aes(y = log_diff_sp5, colour = "Log-difference"), linewidth = 0.8, alpha = 0.8) +
+  # Add line for the percentage change
+  geom_line(aes(y = percent_delta_sp5, colour = "Percentage change"), linewidth = 0.8, alpha = 0.8) +
+  scale_x_date(date_breaks = "20 years", date_labels = "%Y") +
+  xlab(NULL) + ylab(NULL) +
+  ggtitle("Log Differences vs. Actual % Change of S&P500") +
+  # Adjust the colours manually to get nice legend labels
+  scale_colour_manual(
+    values = c("Log-difference" = col_pal[["red"]], "Percentage change" = col_pal[["orange"]])
+  ) +
+  theme_bw() +
+  # tell ggplot where to put the legend
+  theme(legend.position = "inside",
+        legend.position.inside = c(0.9, 0.925),
+        legend.title = element_blank(),
+        legend.background = element_rect(color = NA, fill = NA))
+
+ggplotly(plot_compare_chg, tooltip = c("x", "y", "colour")) %>%
+  layout(legend = list(x = 0, orientation = "h", title = element_blank()))
+
+
+# e) ----------------------------------------------------------------------
+
+sp5 <- sp5 %>%
+  mutate(log_real_sp5 = 100 * (log(sp500) - log(CPI)))
+
+plot_index_real <- sp5 %>%
+  ggplot(aes(x = Date, y = log_real_sp5)) +
+  geom_line(linewidth = 0.8, colour = col_pal[["brightgreen"]]) +
+  scale_x_date(date_breaks = "20 years", date_labels = "%Y") +
+  xlab(NULL) + ylab(NULL) +
+  ggtitle("S&P500 Monthly Index (Real)") +
+  theme_bw()
+
+plot_index_real
+
+plot_index_nom_v_real <- sp5 %>%
+  ggplot(aes(x = Date)) +
+  geom_line(aes(y = log_sp5, colour = "Nominal"), linewidth = 0.8, alpha = 0.8) +
+  geom_line(aes(y = log_real_sp5, colour = "Real"), linewidth = 0.8, alpha = 0.8) +
+  scale_x_date(date_breaks = "20 years", date_labels = "%Y") +
+  xlab(NULL) + ylab(NULL) +
+  ggtitle("S&P500 Monthly Index: Nominal vs Real") +
+  scale_colour_manual(
+    values = c("Nominal" = col_pal[["lightblue"]], "Real" = col_pal[["brightgreen"]])
+  ) +
+  theme_bw() +
+  theme(legend.position = "inside",
+        legend.position.inside = c(0.9, 0.925),
+        legend.title = element_blank(),
+        legend.background = element_rect(color = NA, fill = NA))
+
+plot_index_nom_v_real
